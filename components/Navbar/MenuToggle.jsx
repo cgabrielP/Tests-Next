cb105eaad60d5d57f4eacd6d176191b3017a0a50
Eliseo Fuentes <eliseofuentes.rewired@latam.com>
2023-01-10 23:04:50 -0300

    fixes: add chakra icons and return

--- components/Navbar/MenuToggle.jsx
+++ components/Navbar/MenuToggle.jsx
@@ -1,11 +1,12 @@
-import React from "react"
-import { Box, CloseIcon, HamburgerIcon } from "@chakra-ui/react"
+import React from "react";
+import { Box } from "@chakra-ui/react";
+import { CloseIcon, HamburgerIcon } from "@chakra-ui/icons";
 
 const MenuToggle = ({ toggle, isOpen }) => {
   return (
     <Box display={{ base: "block", md: "none" }} onClick={toggle}>
-      {isOpen ? <CloseIcon/> : <HamburgerIcon />}
+      {isOpen ? <CloseIcon /> : <HamburgerIcon />}
     </Box>
-  )
-}
-export default MenuToggle
+  );
+};
+export default MenuToggle;
--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import NavBarContainer from './NavBarContainer'
-import Logo from './Logo'
-import MenuToggle from './MenuToggle'
-import MenuLinks from './MenuLinks'
+import React from "react";
+import NavBarContainer from "./NavBarContainer";
+import Logo from "./Logo";
+import MenuToggle from "./MenuToggle";
+import MenuLinks from "./MenuLinks";
 
 const NavBar = (props) => {
-  const [isOpen, setIsOpen] = React.useState(false)
+  const [isOpen, setIsOpen] = React.useState(false);
 
-  const toggle = () => setIsOpen(!isOpen)
+  const toggle = () => setIsOpen(!isOpen);
 
   return (
     <NavBarContainer {...props}>
@@ -19,7 +19,7 @@ const NavBar = (props) => {
       <MenuToggle toggle={toggle} isOpen={isOpen} />
       <MenuLinks isOpen={isOpen} />
     </NavBarContainer>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
